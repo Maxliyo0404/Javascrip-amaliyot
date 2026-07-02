c28ea43187a848eaae55aabbe669ae9118922114
Mahliyo <gasandigital13@gmail.com>
2026-07-02 22:44:04 +0500

    amaliyot

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 // let a = 12;
 // let b = 24;
 // console.log(a+b);
-let sum = [1, 4, 8, 9, 10, 5];
+// let sum = [1, 4, 8, 9, 10, 5];
 // let result = sum.filter((element, index)=>{
 //  return index % 2 === 0;
 // })
+
+
+
+let son = [10, 30, 60, 20, 89, 75];
+let 
 console.log(result);
