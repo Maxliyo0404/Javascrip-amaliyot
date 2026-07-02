b6d8b69dad506449e4900f8a4eeb15f0ed05b171
Mahliyo <gasandigital13@gmail.com>
2026-07-02 22:50:19 +0500

    amaliyot

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // let a = 12;
 // let b = 24;
 // console.log(a+b);
+
 // let sum = [1, 4, 8, 9, 10, 5];
 // let result = sum.filter((element, index)=>{
 //  return index % 2 === 0;
@@ -11,3 +12,5 @@
 // let son = [10, 30, 60, 20, 89, 75];
 // let  result = son.filter(n => n > 20);
 // console.log(result);
+
+const
