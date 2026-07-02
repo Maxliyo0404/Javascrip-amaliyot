30fbd705f21aa2f2f3d03d8d2b9ac117a864afa7
Mahliyo <gasandigital13@gmail.com>
2026-07-02 19:47:30 +0500

    amaliyot

--- main.js
+++ main.js
@@ -2,5 +2,7 @@
 // let b = 24;
 // console.log(a+b);
 let sum = [1, 4, 8, 9, 10, 5];
-let result = sum.indexOf(son=> son % 3 === 0);
+let result = sum.filter((element, index)=>){
+ return
+}
 console.log(result);
