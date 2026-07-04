6ba30d617590ae96de3ec760a395c8dccfdfcfb1
Mahliyo <gasandigital13@gmail.com>
2026-07-04 23:23:38 +0500

    map star misollar users

--- main.js
+++ main.js
@@ -71,7 +71,7 @@
 // let number = son.map(n => n * 2);
 // console.log(number);
 const user = [
-   name: "Ali", age: 23,
+  { name: "Ali", age: 23},
   name: "Vali", age: 17,
 ]
 const users =user.map(ism => ism.name);
